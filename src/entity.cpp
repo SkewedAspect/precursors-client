859802bcc04830f5ced0e2c610e168e56e1554bc
David H. Bronke <whitelynx@gmail.com>
2013-07-07 00:28:10 -0500

    Added missing files!

---
+++ src/entity.cpp
@@ -0,0 +1,156 @@
+#include "entity.h"
+
+#include <cmath>
+
+#include <QtGui/QQuaternion>
+
+
+QHash<H3DNode, Entity*> Entity::entities;
+
+
+Entity::Entity() :
+    QObject(), logger(PLogManager::getLogger("entity"))
+{
+	throw new std::exception;
+} // end Entity
+
+Entity::Entity(H3DNode node, QObject *parent) :
+    QObject(parent), _node(node), logger(PLogManager::getLogger("entity"))
+{
+	float x, y, z;
+
+	h3dGetNodeTransform(node,
+			&x, &y, &z,
+			&_pitch, &_heading, &_roll,
+			NULL, NULL, NULL);
+
+	_pos = QVector3D(x, y, z);
+} // end Entity
+
+qreal Entity::yaw() const
+{
+	return _heading;
+} // end yaw
+
+qreal Entity::pitch() const
+{
+	return _pitch;
+} // end pitch
+
+qreal Entity::roll() const
+{
+	return _roll;
+} // end roll
+
+void Entity::setYaw(qreal heading)
+{
+	_heading = heading;
+	apply();
+} // end setYaw
+
+void Entity::setPitch(qreal pitch)
+{
+	_pitch = pitch;
+	apply();
+} // end setPitch
+
+void Entity::setRoll(qreal roll)
+{
+	_roll = roll;
+	apply();
+} // end setRoll
+
+void Entity::changeYaw(qreal dY)
+{
+	_heading += dY;
+	apply();
+} // end changeYaw
+
+void Entity::changePitch(qreal dP)
+{
+	_pitch += dP;
+	apply();
+} // end changePitch
+
+void Entity::changeRoll(qreal dR)
+{
+	_roll += dR;
+	apply();
+} // end changeRoll
+
+void Entity::apply()
+{
+	h3dSetNodeTransform(_node,
+			_pos.x(), _pos.y(), _pos.z(),
+			_pitch, _heading, _roll,
+			1, 1, 1);
+} // end apply
+
+Entity* Entity::getEntity(H3DNode node)
+{
+	if(!entities.contains(node))
+	{
+		entities[node] = new Entity(node);
+	} // end if
+
+	return entities[node];
+} // end getEntity
+
+QQuaternion Entity::eulerToQuat(qreal yaw, qreal pitch, qreal roll)
+{
+    qreal hy = yaw / 2;
+    qreal hp = pitch / 2;
+    qreal hr = roll / 2;
+
+    return QQuaternion(
+            cos(hp) * cos(hr) * cos(hy) + sin(hp) * sin(hr) * sin(hy),
+            sin(hp) * cos(hr) * cos(hy) - cos(hp) * sin(hr) * sin(hy),
+            cos(hp) * sin(hr) * cos(hy) + sin(hp) * cos(hr) * sin(hy),
+            cos(hp) * cos(hr) * sin(hy) - sin(hp) * sin(hr) * cos(hy)
+            );
+} // end eulerToQuat
+
+qreal Entity::matrixToYaw(QMatrix4x4 mat)
+{
+	if(mat(1, 2) > 0.9999999998 || mat(1, 2) < -0.9999999998)
+	{
+		return atan2(-mat(2, 0), mat(0, 0));
+	}
+	else
+	{
+		return atan2(mat(0, 2), mat(2, 2));
+	} // end if
+} // end eulerToQuat
+
+qreal Entity::matrixToPitch(QMatrix4x4 mat)
+{
+	return asin(mat(1, 2));
+} // end eulerToQuat
+
+qreal Entity::matrixToRoll(QMatrix4x4 mat)
+{
+	if(mat(1, 2) > 0.9999999998 || mat(1, 2) < -0.9999999998)
+	{
+		return 0;
+	}
+	else
+	{
+		return atan2(mat(1, 0), mat(1, 1));
+	} // end if
+} // end eulerToQuat
+
+void Entity::matrixToEuler(QMatrix4x4 mat, qreal *yaw, qreal *pitch, qreal *roll)
+{
+	if(mat(1, 2) > 0.9999999998 || mat(1, 2) < -0.9999999998)
+	{
+		*yaw = atan2(-mat(2, 0), mat(0, 0));
+		*roll = 0;
+	}
+	else
+	{
+		*yaw = atan2(mat(0, 2), mat(2, 2));
+		*roll = atan2(mat(1, 0), mat(1, 1));
+	} // end if
+
+	*pitch = asin(mat(1, 2));
+} // end eulerToQuat
---
+++ src/entity.h
@@ -0,0 +1,62 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <QtCore/QObject>
+#include <QtGui/QMatrix4x4>
+#include <QtGui/QVector3D>
+
+#include <Horde3D.h>
+
+#include "plogging/plogging.h"
+
+
+class Entity : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(qreal yaw READ yaw WRITE setYaw)
+    Q_PROPERTY(qreal pitch READ pitch WRITE setPitch)
+    Q_PROPERTY(qreal roll READ roll WRITE setRoll)
+
+public:
+    Entity();
+
+    H3DNode node() const { return _node; }
+
+    qreal yaw() const;
+    qreal pitch() const;
+    qreal roll() const;
+
+    void setYaw(qreal y);
+    void setPitch(qreal p);
+    void setRoll(qreal r);
+
+	Q_INVOKABLE void changeYaw(qreal dY);
+	Q_INVOKABLE void changePitch(qreal dP);
+	Q_INVOKABLE void changeRoll(qreal dR);
+
+    Q_INVOKABLE void apply();
+
+	Q_INVOKABLE static Entity* getEntity(H3DNode node);
+
+	static QQuaternion eulerToQuat(qreal yaw, qreal pitch, qreal roll);
+	static void matrixToEuler(QMatrix4x4 mat, qreal *yaw, qreal *pitch, qreal *roll);
+
+	static qreal matrixToYaw(QMatrix4x4 mat);
+	static qreal matrixToPitch(QMatrix4x4 mat);
+	static qreal matrixToRoll(QMatrix4x4 mat);
+
+protected:
+    explicit Entity(H3DNode node, QObject *parent = 0);
+
+private:
+    H3DNode _node;
+
+	QVector3D _pos;
+	float _heading, _pitch, _roll;
+
+	PLogger& logger;
+
+	static QHash<H3DNode, Entity*> entities;
+}; // end Entity
+
+#endif // ENTITY_H
