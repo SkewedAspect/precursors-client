157dc80f5c507121c3f8c5df3b8fae9c60b6d051
Christopher S. Case <chris.case@g33xnexus.com>
2013-07-10 12:00:00 -0500

    added a little bit of filtering on the signal.

--- resources/js/entity_manager.js
+++ resources/js/entity_manager.js
@@ -19,9 +19,14 @@ EntityManager.prototype.connectSignals = function()
     networking.incommingMessage.connect(this.handleIncomingEvent);
 } // end connectSignals
 
-EntityManager.prototype.handleIncomingEvent = function(event)
+EntityManager.prototype.handleIncomingEvent = function(channel, event)
 {
-    logger.debug("Incoming Event: %1", JSON.stringify(event));
+    logger.debug("Incoming Event: %1::%2", channel, JSON.stringify(event));
+
+	if(channel == "entity")
+	{
+		logger.debug("Incoming Entity Event: %1", JSON.stringify(event));
+	} // end if
 } // end handleIncomingEvent
 
 // ---------------------------------------------------------------------------------------------------------------------
--- resources/js/level_manager.js
+++ resources/js/level_manager.js
@@ -19,22 +19,25 @@ LevelManager.prototype.connectSignals = function()
     networking.incommingMessage.connect(this.handleIncomingEvent);
 } // end connectSignals
 
-LevelManager.prototype.handleIncomingEvent = function(event)
+LevelManager.prototype.handleIncomingEvent = function(channel, event)
 {
-	if(event.type == "setZone")
+	if(channel == "level")
 	{
-		logger.debug("Incoming setZone: %1", JSON.stringify(event.level));
-
-		//XXX: HACK! This is here because the server sends us a hard-coded value for the level. This is the correct level
-		// for the python client, but not for us.
-		if(event.level == "zones/test/TestArea.json")
+		if(event.type == "setZone")
 		{
-			logger.warning("Got old test level; loading current test zone instead.");
-			event.level = "resources/scenes/asteroids/asteroids.scene.xml";
-		} // end if
+			logger.debug("Incoming setZone: %1", JSON.stringify(event.level));
 
-		// TODO: load the level?
-		logger.debug("Would load level \'%1\' here!", event.level);
+			//XXX: HACK! This is here because the server sends us a hard-coded value for the level. This is the correct level
+			// for the python client, but not for us.
+			if(event.level == "zones/test/TestArea.json")
+			{
+				logger.warning("Got old test level; loading current test zone instead.");
+				event.level = "resources/scenes/asteroids/asteroids.scene.xml";
+			} // end if
+
+			// TODO: load the level?
+			logger.debug("Would load level \'%1\' here!", event.level);
+		} // end if
 	} // end if
 } // end handleIncomingEvent
 
