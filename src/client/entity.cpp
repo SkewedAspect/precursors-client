26f747bc0e87db3e2c601f6a3476872100bef9e4
David H. Bronke <whitelynx@gmail.com>
2013-07-12 10:54:02 -0500

    Added Entity::toString(), added 'parent' property to Entity, updated 'pos' property to use getters and setters, added NOTIFY signals to heading, pitch, and roll, and changed signals to pass the updated value.

--- src/client/entity.cpp
+++ src/client/entity.cpp
@@ -9,110 +9,130 @@ QList<Entity*> Entity::scheduledRepeatingEntities;
 
 
 Entity::Entity() :
-    QObject(), _node(0), scheduledOnce(false), scheduledRepeating(false),
-    _logger(PLogManager::getLogger("entity")), _mgr(Horde3DManager::instance())
+	QObject(), _node(0), scheduledOnce(false), scheduledRepeating(false),
+	_logger(PLogManager::getLogger("entity")), _mgr(Horde3DManager::instance())
 {
-    throw new std::exception;
+	throw new std::exception;
 } // end Entity
 
 Entity::Entity(H3DNode node, QObject *parent) :
-    QObject(parent), _node(node), scheduledOnce(false), scheduledRepeating(false),
-    _logger(PLogManager::getLogger("entity")), _mgr(Horde3DManager::instance())
+	QObject(parent), _node(node), scheduledOnce(false), scheduledRepeating(false),
+	_logger(PLogManager::getLogger("entity")), _mgr(Horde3DManager::instance())
 {
-    float x, y, z;
+	float x, y, z;
 
-    h3dGetNodeTransform(node,
-            &x, &y, &z,
-            &_pitch, &_heading, &_roll,
-            NULL, NULL, NULL);
+	h3dGetNodeTransform(node,
+			&x, &y, &z,
+			&_pitch, &_heading, &_roll,
+			NULL, NULL, NULL);
 
-    _pos = QVector3D(x, y, z);
+	_pos = QVector3D(x, y, z);
 } // end Entity
 
 
 qreal Entity::heading() const
 {
-    return _heading;
+	return _heading;
 } // end heading
 
 qreal Entity::pitch() const
 {
-    return _pitch;
+	return _pitch;
 } // end pitch
 
 qreal Entity::roll() const
 {
-    return _roll;
+	return _roll;
 } // end roll
+
+QVector3D Entity::pos() const
+{
+	return _pos;
+} // end pos
+
+Entity* Entity::parent() const
+{
+	return _parent;
+} // end parent
 
 void Entity::setPos(qreal x, qreal y, qreal z)
 {
-	this->_pos.setX(x);
-	this->_pos.setY(y);
-	this->_pos.setZ(z);
-
-	emit posChanged();
+	setPos(QVector3D(x, y, z));
 } // end setPos
 
 void Entity::setState(QString key, QVariant value)
 {
-	this->_state[key] = value;
+	_state[key] = value;
 
 	emit stateChanged();
 } // end setState
 
 void Entity::updateState(QVariantMap delta)
 {
-	for( QMap<QString, QVariant>::iterator it = delta.begin(); it != delta.end(); )
+	for(QVariantMap::iterator it = delta.begin(); it != delta.end(); )
 	{
-		this->_state.insert(it.key(), it.value());
+		_state.insert(it.key(), it.value());
 		it++;
-    } // end for
+	} // end for
 
 	emit stateChanged();
 } // end updateState
 
 QVariant Entity::getState(QString key, QVariant defaultValue)
 {
-	return this->_state.value(key, defaultValue);
+	return _state.value(key, defaultValue);
 } // end getState
 
 void Entity::setHeading(qreal heading)
 {
-    _heading = heading;
-    scheduleOnce();
+	_heading = heading;
+	emit headingChanged(heading);
+	scheduleOnce();
 } // end setHeading
 
 void Entity::setPitch(qreal pitch)
 {
-    _pitch = pitch;
-    scheduleOnce();
+	_pitch = pitch;
+	emit pitchChanged(pitch);
+	scheduleOnce();
 } // end setPitch
 
 void Entity::setRoll(qreal roll)
 {
-    _roll = roll;
-    scheduleOnce();
+	_roll = roll;
+	emit rollChanged(roll);
+	scheduleOnce();
 } // end setRoll
 
-
-void Entity::changeHeading(qreal dY)
+void Entity::setPos(QVector3D pos)
 {
-    _heading += dY;
-    scheduleOnce();
-} // end changeHeading
+	_pos = pos;
+	emit posChanged(pos);
+	scheduleOnce();
+} // end setRoll
 
-void Entity::changePitch(qreal dP)
+void Entity::setParent(Entity* parent)
 {
-    _pitch += dP;
-    scheduleOnce();
-} // end changePitch
+	if(parent == NULL)
+	{
+		if(!h3dSetNodeParent(_node, _mgr.root()->node()))
+		{
+			_logger.error(QString("Failed to unset %1's parent!").arg(toString()));
+			return;
+		} // end if
+	}
+	else
+	{
+		if(!h3dSetNodeParent(_node, parent->node()))
+		{
+			_logger.error(QString("Failed to set %1's parent to %2!").arg(toString()).arg(parent->toString()));
+			return;
+		} // end if
+	} // end if
 
-void Entity::changeRoll(qreal dR)
-{
-    _roll += dR;
-    scheduleOnce();
-} // end changeRoll
+	_parent = parent;
+	emit parentChanged(parent);
+} // end setRoll
 
 
 QList<Entity*> Entity::find(QString childName)
@@ -131,49 +151,49 @@ QList<Entity*> Entity::find(QString childName)
 
 Entity* Entity::newGroup(QString groupName)
 {
-    H3DNode groupNode = h3dAddGroupNode(_node, groupName.toUtf8().constData());
+	H3DNode groupNode = h3dAddGroupNode(_node, groupName.toUtf8().constData());
 	if(groupNode == 0)
 	{
 		_logger.error(QString("Failed to create group %1!").arg(groupName));
 	} // end if
 
-    return Entity::getEntity(groupNode);
+	return Entity::getEntity(groupNode);
 } // end newGroup
 
 Entity* Entity::newCamera(QString cameraName, QString pipelineName)
 {
-    H3DRes pipeline = _mgr.loadPipeline(pipelineName);
+	H3DRes pipeline = _mgr.loadPipeline(pipelineName);
 	if(pipeline == 0)
 	{
 		_logger.error(QString("Failed to load pipeline %1!").arg(pipelineName));
 	} // end if
 
-    H3DNode camera = h3dAddCameraNode(_node, cameraName.toUtf8().constData(), pipeline);
+	H3DNode camera = h3dAddCameraNode(_node, cameraName.toUtf8().constData(), pipeline);
 	if(camera == 0)
 	{
 		_logger.error(QString("Failed to create camera %1!").arg(cameraName));
 	} // end if
 
-    h3dSetNodeParamF(camera, H3DCamera::FarPlaneF, 0, 100000);
+	h3dSetNodeParamF(camera, H3DCamera::FarPlaneF, 0, 100000);
 
-    //FIXME: Get rid of this! (and replace it with getters/setters for pos, x, y, and z)
-    h3dSetNodeTransform(camera,
-            0, 0, 100,
-            0, 0, 0,
-            1, 1, 1
-            );
+	//FIXME: Get rid of this! (and replace it with getters/setters for pos, x, y, and z)
+	h3dSetNodeTransform(camera,
+			0, 0, 100,
+			0, 0, 0,
+			1, 1, 1
+			);
 
-    return Entity::getEntity(camera);
+	return Entity::getEntity(camera);
 } // end newCamera
 
 Entity* Entity::loadModel(QString modelPath, int flags)
 {
-    return loadEntityFromRes(H3DResTypes::SceneGraph, modelPath, flags);
+	return loadEntityFromRes(H3DResTypes::SceneGraph, modelPath, flags);
 } // end loadModel
 
 Entity* Entity::loadScene(QString scenePath, int flags)
 {
-    Entity* scene = loadModel(scenePath, flags);
+	Entity* scene = loadModel(scenePath, flags);
 
 	// Apply NoCastShadow to all skyboxes.
 	QList<Entity*> skyboxes = scene->find("skybox");
@@ -187,92 +207,98 @@ Entity* Entity::loadScene(QString scenePath, int flags)
 
 Entity* Entity::loadEntityFromRes(H3DResTypes::List type, QString path, int flags)
 {
-    H3DRes res = h3dAddResource(type, path.toUtf8().constData(), flags);
+	H3DRes res = h3dAddResource(type, path.toUtf8().constData(), flags);
 	if(res == 0)
 	{
 		_logger.error(QString("Failed to create resource handle for %1!").arg(path));
 	} // end if
 
-    if(!h3dIsResLoaded(res))
-    {
+	if(!h3dIsResLoaded(res))
+	{
 		_logger.debug(QString("Loading resources from: %1").arg(_mgr.contentDirs()));
 		if(!_mgr.loadNewResources())
-        {
+		{
 			_logger.error(QString("Failed to load resource %1!").arg(path));
-            return NULL;
-        } // end if
-    } // end if
+			return NULL;
+		} // end if
+	} // end if
 
-    H3DNode newNode = h3dAddNodes(_node, res);
+	H3DNode newNode = h3dAddNodes(_node, res);
 	if(newNode == 0)
 	{
 		_logger.error(QString("Failed to create entity node for resource %1!").arg(path));
 	} // end if
 
-    return Entity::getEntity(newNode);
+	return Entity::getEntity(newNode);
 } // end loadEntityFromRes
 
 
 void Entity::scheduleOnce()
 {
-    if(!scheduledOnce && !scheduledRepeating)
-    {
-        scheduledOnceEntities.append(this);
-    } // end if
+	if(!scheduledOnce && !scheduledRepeating)
+	{
+		scheduledOnceEntities.append(this);
+	} // end if
 
-    scheduledOnce = true;
+	scheduledOnce = true;
 } // end scheduleOnce
 
 void Entity::scheduleRepeating()
 {
-    if(!scheduledRepeating)
-    {
-        scheduledRepeatingEntities.append(this);
+	if(!scheduledRepeating)
+	{
+		scheduledRepeatingEntities.append(this);
 
-        scheduledRepeating = true;
-    } // end if
+		scheduledRepeating = true;
+	} // end if
 } // end scheduleRepeating
 
 void Entity::stopRepeating()
 {
-    scheduledRepeatingEntities.removeAll(this);
+	scheduledRepeatingEntities.removeAll(this);
 } // end scheduleRepeating
 
 void Entity::apply()
 {
-    h3dSetNodeTransform(_node,
-            _pos.x(), _pos.y(), _pos.z(),
-            _pitch, _heading, _roll,
-            1, 1, 1);
+	h3dSetNodeTransform(_node,
+			_pos.x(), _pos.y(), _pos.z(),
+			_pitch, _heading, _roll,
+			1, 1, 1);
 
-    if(scheduledOnce)
-    {
-        scheduledOnce = false;
-    } // end if
+	if(scheduledOnce)
+	{
+		scheduledOnce = false;
+	} // end if
 } // end apply
+
+
+QString Entity::toString()
+{
+	return QString("<Entity %1>").arg(int(_node));
+} // end toString
 
 
 // Static Methods //
 
 void Entity::runScheduled()
 {
-    while(!scheduledOnceEntities.isEmpty())
-    {
-        scheduledOnceEntities.takeFirst()->apply();
-    } // end while
+	while(!scheduledOnceEntities.isEmpty())
+	{
+		scheduledOnceEntities.takeFirst()->apply();
+	} // end while
 
-    for(int i = 0; i < scheduledRepeatingEntities.size(); i++)
-    {
-        scheduledRepeatingEntities.at(i)->apply();
-    } // end for
+	for(int i = 0; i < scheduledRepeatingEntities.size(); i++)
+	{
+		scheduledRepeatingEntities.at(i)->apply();
+	} // end for
 } // end runScheduled
 
 Entity* Entity::getEntity(H3DNode node)
 {
-    if(!entities.contains(node))
-    {
-        entities[node] = new Entity(node);
-    } // end if
+	if(!entities.contains(node))
+	{
+		entities[node] = new Entity(node);
+	} // end if
 
-    return entities[node];
+	return entities[node];
 } // end getEntity
--- src/client/entity.h
+++ src/client/entity.h
@@ -15,77 +15,85 @@
 
 class Entity : public QObject
 {
-    Q_OBJECT
-    Q_PROPERTY(qreal heading READ heading WRITE setHeading)
-    Q_PROPERTY(qreal pitch READ pitch WRITE setPitch)
-    Q_PROPERTY(qreal roll READ roll WRITE setRoll)
-	Q_PROPERTY(QVector3D pos MEMBER _pos NOTIFY posChanged)
+	Q_OBJECT
+	Q_PROPERTY(qreal heading READ heading WRITE setHeading NOTIFY headingChanged)
+	Q_PROPERTY(qreal pitch READ pitch WRITE setPitch NOTIFY pitchChanged)
+	Q_PROPERTY(qreal roll READ roll WRITE setRoll NOTIFY rollChanged)
+	Q_PROPERTY(QVector3D pos READ pos WRITE setPos NOTIFY posChanged)
+	Q_PROPERTY(Entity* parent READ parent WRITE setParent NOTIFY parentChanged)
 	Q_PROPERTY(QVariantMap state MEMBER _state NOTIFY stateChanged)
 
 public:
-    Entity();
+	Entity();
 
-    H3DNode node() const { return _node; }
+	H3DNode node() const { return _node; }
 
-    qreal heading() const;
-    qreal pitch() const;
-    qreal roll() const;
+	qreal heading() const;
+	qreal pitch() const;
+	qreal roll() const;
+	QVector3D pos() const;
+	Entity* parent() const;
 
-    void setHeading(qreal y);
-    void setPitch(qreal p);
-    void setRoll(qreal r);
+	void setHeading(qreal heading);
+	void setPitch(qreal pitch);
+	void setRoll(qreal roll);
+	void setPos(QVector3D pos);
+	void setParent(Entity* parent);
 
-    Q_INVOKABLE void setPos(qreal x, qreal y, qreal z);
+	Q_INVOKABLE void setPos(qreal x, qreal y, qreal z);
 
 	Q_INVOKABLE void setState(QString key, QVariant value);
 	Q_INVOKABLE void updateState(QVariantMap delta);
 	Q_INVOKABLE QVariant getState(QString key, QVariant defaultValue);
 
-    Q_INVOKABLE void changeHeading(qreal dY);
-    Q_INVOKABLE void changePitch(qreal dP);
-    Q_INVOKABLE void changeRoll(qreal dR);
-
 	Q_INVOKABLE QList<Entity*> find(QString childName);
 
-    Q_INVOKABLE Entity* newCamera(QString cameraName, QString pipelineName = QString());
-    Q_INVOKABLE Entity* newGroup(QString groupName);
-    Q_INVOKABLE Entity* loadModel(QString scenePath, int flags = 0);
-    Q_INVOKABLE Entity* loadScene(QString scenePath, int flags = 0);
-    Q_INVOKABLE Entity* loadEntityFromRes(H3DResTypes::List type, QString path, int flags = 0);
+	Q_INVOKABLE Entity* newCamera(QString cameraName, QString pipelineName = QString());
+	Q_INVOKABLE Entity* newGroup(QString groupName);
+	Q_INVOKABLE Entity* loadModel(QString scenePath, int flags = 0);
+	Q_INVOKABLE Entity* loadScene(QString scenePath, int flags = 0);
+	Q_INVOKABLE Entity* loadEntityFromRes(H3DResTypes::List type, QString path, int flags = 0);
 
-    Q_INVOKABLE void scheduleOnce();
-    Q_INVOKABLE void scheduleRepeating();
-    Q_INVOKABLE void stopRepeating();
+	Q_INVOKABLE void scheduleOnce();
+	Q_INVOKABLE void scheduleRepeating();
+	Q_INVOKABLE void stopRepeating();
 
-    Q_INVOKABLE static Entity* getEntity(H3DNode node);
-    Q_INVOKABLE static void runScheduled();
+	Q_INVOKABLE static Entity* getEntity(H3DNode node);
+	Q_INVOKABLE static void runScheduled();
 
-    void apply();
+	void apply();
+
+	Q_INVOKABLE QString toString();
 
 signals:
+	void headingChanged(qreal heading);
+	void pitchChanged(qreal pitch);
+	void rollChanged(qreal roll);
+	void posChanged(QVector3D pos);
+	void parentChanged(Entity* parent);
 	void stateChanged();
-	void posChanged();
 
 protected:
-    explicit Entity(H3DNode node, QObject *parent = 0);
+	explicit Entity(H3DNode node, QObject *parent = 0);
 
 private:
-    H3DNode _node;
+	H3DNode _node;
+	Entity* _parent;
 
 	QVariantMap _state;
 
-    QVector3D _pos;
-    float _heading, _pitch, _roll;
+	QVector3D _pos;
+	float _heading, _pitch, _roll;
 
-    bool scheduledOnce;
-    bool scheduledRepeating;
+	bool scheduledOnce;
+	bool scheduledRepeating;
 
-    PLogger& _logger;
-    Horde3DManager& _mgr;
+	PLogger& _logger;
+	Horde3DManager& _mgr;
 
-    static QHash<H3DNode, Entity*> entities;
-    static QList<Entity*> scheduledOnceEntities;
-    static QList<Entity*> scheduledRepeatingEntities;
+	static QHash<H3DNode, Entity*> entities;
+	static QList<Entity*> scheduledOnceEntities;
+	static QList<Entity*> scheduledRepeatingEntities;
 }; // end Entity
 
 #endif // ENTITY_H
