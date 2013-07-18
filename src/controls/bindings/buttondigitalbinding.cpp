80d4423553655d53ded2f33d1ab65e843b1fd183
Christopher S. Case <chris.case@g33xnexus.com>
2013-07-17 21:49:29 -0500

    Implemented ButtonDigitalBinding.

--- src/controls/bindings/buttondigitalbinding.cpp
+++ src/controls/bindings/buttondigitalbinding.cpp
@@ -1,11 +1,40 @@
 #include "buttondigitalbinding.h"
 
+/**********************************************************************************************************************/
+/* Public API                                                                                                         */
+/**********************************************************************************************************************/
 
 ButtonDigitalBinding::ButtonDigitalBinding(QObject *parent) :
-    ControlBinding(parent)
+    ControlBinding(parent),
+    _invert(false),
+    _toggle(false),
+    _state(false)
 {
-}
+} // end ButtonDigitalBinding
+
+ButtonDigitalBinding::state()
+{
+    return _state;
+} // end state
+
+/**********************************************************************************************************************/
+/* Slots                                                                                                              */
+/**********************************************************************************************************************/
 
 void ButtonDigitalBinding::onSignalUpdated(bool pressed, bool repeating)
 {
-}
+    if(_toggle)
+    {
+        // Only toggle if our last state was not pressed, and our current state is pressed.
+        if(!_lastPressedState && pressed)
+        {
+            _state = !_state;
+        } // end if
+    }
+    else
+    {
+        _state = (pressed != _invert);
+    } // end if
+
+    _lastPressedState = pressed;
+} // end onSignalUpdated
--- src/controls/bindings/buttondigitalbinding.h
+++ src/controls/bindings/buttondigitalbinding.h
@@ -7,16 +7,27 @@
 class ButtonDigitalBinding : public ControlBinding
 {
     Q_OBJECT
+    Q_PROPERTY(bool state READ state NOTIFY stateChanged)
+    Q_PROPERTY(bool invert MEMBER _invert NOTIFY invertChanged)
+    Q_PROPERTY(bool toggle MEMBER _toggle NOTIFY toggleChanged)
 
 public:
     explicit ButtonDigitalBinding(QObject *parent = 0);
 
-    bool toggle;
-    bool invert;
-    bool state;	// ReadOnly
+    bool state();
+
+signals:
+    void invertChanged();
+    void toggleChanged();
 
 public slots:
     void onSignalUpdated(bool pressed, bool repeating);
+
+private:
+    bool _invert;
+    bool _toggle;
+    bool _state;
+    bool _lastPressedState;
 };
 
 #endif // BUTTONDIGITALBINDING_H
