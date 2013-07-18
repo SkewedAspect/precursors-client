5b085de783bffbfa1d6966f9ad5c5db6a42b1a3b
Christopher S. Case <chris.case@g33xnexus.com>
2013-07-18 13:38:53 -0500

    implemented AxisDigitalBinding.

--- src/controls/bindings/axisdigitalbinding.cpp
+++ src/controls/bindings/axisdigitalbinding.cpp
@@ -1,11 +1,51 @@
 #include "axisdigitalbinding.h"
 
 
+/**********************************************************************************************************************/
+/* Public API                                                                                                         */
+/**********************************************************************************************************************/
+
 AxisDigitalBinding::AxisDigitalBinding(QObject *parent) :
-    ControlBinding(parent)
+    ControlBinding(parent),
+    _threshold(0),
+    _overlap(0),
+    _invert(false),
+    _state(false)
 {
-}
+    // Annoyingly, `Q_PROPERTY` doesn't work with signals defined in the base class. So, we cheat.
+    connect(this, SIGNAL(stateChanged()), parent, SIGNAL(stateChanged()));
+} // end AxisDigitlBinding
+
+bool AxisDigitalBinding::state()
+{
+   return this->_state;
+} // end state
+
+/**********************************************************************************************************************/
+/* Slots                                                                                                              */
+/**********************************************************************************************************************/
 
 void AxisDigitalBinding::onSignalUpdated(float position)
 {
-}
+    bool previousState = _state;
+
+    if(previousState != _invert)
+    {
+         // If either we were previously active and `invert` is False, or if we were inactive and `invert` is True,
+         // then change if we fall below `threshold` - `overlap`.
+        if(position < (_threshold - _overlap))
+        {
+            _state = !previousState;
+            emit stateChanged();
+        } // end if
+    }
+    else
+    {
+        // Otherwise, change if we go above `threshold`.
+        if(position > _threshold)
+        {
+            _state = !previousState;
+            emit stateChanged();
+        } // end if
+    } // end if
+} // end onSignalUpdated
--- src/controls/bindings/axisdigitalbinding.h
+++ src/controls/bindings/axisdigitalbinding.h
@@ -7,17 +7,27 @@
 class AxisDigitalBinding : public ControlBinding
 {
     Q_OBJECT
+    Q_PROPERTY(bool state READ state NOTIFY stateChanged)
+    Q_PROPERTY(bool invert MEMBER _invert)
+    Q_PROPERTY(float threshold MEMBER _threshold)
+    Q_PROPERTY(float  overlap MEMBER _overlap)
 
 public:
     explicit AxisDigitalBinding(QObject *parent = 0);
 
-    float threshold;
-    float overlap;
-    bool invert;
-    bool state;	// Read only
+    bool state();
+
+signals:
+    void stateChanged();
 
 public slots:
     void onSignalUpdated(float position);
+
+ private:
+    float _threshold;
+    float _overlap;
+    bool _invert;
+    bool _state;
 };
 
 #endif // AXISDIGITALBINDING_H
