fe4a74f585c89e241a4781d3137b332c50ab1266
David H. Bronke <whitelynx@gmail.com>
2013-07-24 01:55:26 -0500

    Implemented the beginnings of basic mouse handling in QtDriver.

--- src/controls/qt/qtdriver.cpp
+++ src/controls/qt/qtdriver.cpp
@@ -1,10 +1,16 @@
+#include <cmath>
+
+#include <QMouseEvent>
+#include <QScreen>
 #include <QWindow>
 
 #include "qtdriver.h"
 
 
 QtDriver::QtDriver() :
-    InputDriver()
+		InputDriver(),
+		_logger(PLogManager::getLogger("QtDriver")),
+		_settings(PSettingsManager::instance())
 {
 } // end QtDriver
 
@@ -16,4 +22,44 @@ QString QtDriver::name()
 void QtDriver::setWindow(QWindow* window)
 {
 	_window = window;
+
+	screenSize = _window->screen()->size();
+	connect(_window, SIGNAL(screenChanged(QScreen*)), this, SLOT(onScreenChanged(QScreen*)));
+
+	connect(_window, SIGNAL(mouseMoved(QMouseEvent*, QPoint)), this, SLOT(onMouseMoved(QMouseEvent*, QPoint)));
+	connect(_window, SIGNAL(mousePressed(QMouseEvent*)), this, SLOT(onMousePressed(QMouseEvent*)));
+	connect(_window, SIGNAL(mouseReleased(QMouseEvent*)), this, SLOT(onMouseReleased(QMouseEvent*)));
 } // end setWindow
+
+void QtDriver::onScreenChanged(QScreen* screen)
+{
+	screenSize = screen->size();
+} // end onScreenChanged
+
+void QtDriver::onMouseMoved(QMouseEvent* event, QPoint screenDelta)
+{
+
+	float divisor = fmax(screenSize.width(), screenSize.height());
+
+	float deltaX = screenDelta.x() / divisor;
+	float deltaY = screenDelta.y() / divisor;
+
+	_logger.debug(QString("Mouse moved: %1, %2 (%3, %4)")
+			.arg(deltaX).arg(deltaY)
+			.arg(screenDelta.x()).arg(screenDelta.y())
+			);
+} // end onMouseMoved
+
+void QtDriver::onMousePressed(QMouseEvent* event)
+{
+	_logger.debug(QString("Mouse button %1 pressed.")
+			.arg(event->button())
+			);
+} // end onMousePressed
+
+void QtDriver::onMouseReleased(QMouseEvent* event)
+{
+	_logger.debug(QString("Mouse button %1 released.")
+			.arg(event->button())
+			);
+} // end onMouseReleased
--- src/controls/qt/qtdriver.h
+++ src/controls/qt/qtdriver.h
@@ -1,7 +1,16 @@
 #ifndef QTDRIVER_H
 #define QTDRIVER_H
 
+#include <QSize>
+#include <QPoint>
+
+#include "plogging/plogging.h"
+#include "psettings/psettings.h"
 #include "controls/inputdriver.h"
+
+
+class QMouseEvent;
+class QScreen;
 
 
 class QtDriver : public InputDriver
@@ -17,6 +26,19 @@ public:
 	virtual QString name();
 
     virtual void setWindow(QWindow* window);
+
+public slots:
+	void onScreenChanged(QScreen* screen);
+
+	void onMouseMoved(QMouseEvent* event, QPoint screenDelta);
+	void onMousePressed(QMouseEvent* event);
+	void onMouseReleased(QMouseEvent* event);
+
+protected:
+	QSize screenSize;
+
+	PLogger& _logger;
+	PSettingsManager& _settings;
 };
 
 #endif // QTDRIVER_H
