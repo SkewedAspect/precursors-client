511488ea8525c4d6d12b47f26a81fa05f9731398
David H. Bronke <whitelynx@gmail.com>
2013-08-21 21:03:43 -0500

    Implemented separate calibration for each axis signal, removing the need for the hackish default sensitivity value, and partly fixing mouse input.

--- src/controls/signals/axisinputsignal.cpp
+++ src/controls/signals/axisinputsignal.cpp
@@ -1,9 +1,17 @@
+#include "controls/devices/inputdevice.h"
+
 #include "axisinputsignal.h"
 
 
 AxisInputSignal::AxisInputSignal(InputDevice* device, QString name, QString description) :
+		_settings(PSettingsManager::instance()),
 		InputSignal(device, name, description)
 {
+	QVariantMap calibrationConfig = calibration();
+	_centerValue = calibrationConfig.value("centerValue", 0).toDouble();
+	_deadZone = calibrationConfig.value("deadZone", 0).toDouble();
+	_maxValue = calibrationConfig.value("maxValue", 1).toDouble();
+	_minValue = calibrationConfig.value("minValue", -1).toDouble();
 } // end AxisInputSignal
 
 const InputSignal::Type AxisInputSignal::type() const
@@ -11,7 +19,74 @@ const InputSignal::Type AxisInputSignal::type() const
 	return InputSignal::AXIS;
 } // end type
 
+float AxisInputSignal::centerValue() const
+{
+	return _centerValue;
+} // end centerValue
+
+float AxisInputSignal::deadZone() const
+{
+	return _deadZone;
+} // end deadZone
+
+float AxisInputSignal::maxValue() const
+{
+	return _maxValue;
+} // end maxValue
+
+float AxisInputSignal::minValue() const
+{
+	return _minValue;
+} // end minValue
+
+QVariantMap AxisInputSignal::calibration() const
+{
+	return device()->config()["calibration"]
+			.toMap()[name()]
+			.toMap();
+} // end calibration
+
+void AxisInputSignal::setCenterValue(float value)
+{
+	_centerValue = value;
+} // end setCenterValue
+
+void AxisInputSignal::setDeadZone(float value)
+{
+	_deadZone = value;
+} // end setDeadZone
+
+void AxisInputSignal::setMaxValue(float value)
+{
+	_maxValue = value;
+} // end setMaxValue
+
+void AxisInputSignal::setMinValue(float value)
+{
+	_minValue = value;
+} // end setMinValue
+
 void AxisInputSignal::emitUpdated(float position)
 {
+	position -= _centerValue;
+
+	float max = _maxValue;
+
+	if(position < 0)
+	{
+		position *= -1;
+		max = _minValue;
+	} // end if
+
+	if(position < _deadZone)
+	{
+		position = 0;
+	}
+	else
+	{
+		position -= _deadZone;
+		position /= max;
+	} // end if
+
 	emit updated(position);
 } // end emitUpdated
--- src/controls/signals/axisinputsignal.h
+++ src/controls/signals/axisinputsignal.h
@@ -1,6 +1,10 @@
 #ifndef AXISINPUTSIGNAL_H
 #define AXISINPUTSIGNAL_H
 
+#include <QVariantMap>
+
+#include "psettings/psettings.h"
+
 #include "inputsignal.h"
 
 
@@ -12,11 +16,29 @@ public:
     explicit AxisInputSignal(InputDevice* device, QString name, QString description = QString());
 
 	virtual const InputSignal::Type type() const;
+    float centerValue() const;
+    float deadZone() const;
+    float maxValue() const;
+    float minValue() const;
+	QVariantMap calibration() const;
+
+    void setCenterValue(float value);
+    void setDeadZone(float value);
+    void setMaxValue(float value);
+    void setMinValue(float value);
 
     void emitUpdated(float position);
 
 signals:
     void updated(float position);
+
+private:
+    float _centerValue;
+    float _deadZone;
+    float _maxValue;
+    float _minValue;
+
+	PSettingsManager& _settings;
 };
 
 #endif // AXISINPUTSIGNAL_H
