1861e4ceb1c5e4f8e2b261f8ae017c0be78a1d16
David H. Bronke <whitelynx@gmail.com>
2013-09-04 21:43:14 -0500

    Corrected quatToHPR, removed quatToHeading, quatToPitch, and quatToRoll, added overload of quatToHPR that's invokable from QML, and added several other useful methods to Math3D.

--- src/client/math3d.cpp
+++ src/client/math3d.cpp
@@ -4,62 +4,34 @@
 #include "math3d.h"
 
 
+/**
+ * Based on: http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToQuaternion/index.htm
+ */
 QQuaternion Math3D::eulerToQuat(qreal heading, qreal pitch, qreal roll)
 {
-    qreal hy = heading / 2;
+    qreal hh = heading / 2;
     qreal hp = pitch / 2;
     qreal hr = roll / 2;
 
     return QQuaternion(
-            cos(hp) * cos(hr) * cos(hy) + sin(hp) * sin(hr) * sin(hy),
-            sin(hp) * cos(hr) * cos(hy) - cos(hp) * sin(hr) * sin(hy),
-            cos(hp) * sin(hr) * cos(hy) + sin(hp) * cos(hr) * sin(hy),
-            cos(hp) * cos(hr) * sin(hy) - sin(hp) * sin(hr) * cos(hy)
+            cos(hh) * cos(hp) * cos(hr) - sin(hh) * sin(hp) * sin(hr),
+            sin(hh) * sin(hp) * cos(hr) + cos(hh) * cos(hp) * sin(hr),
+            sin(hh) * cos(hp) * cos(hr) + cos(hh) * sin(hp) * sin(hr),
+            cos(hh) * sin(hp) * cos(hr) - sin(hh) * cos(hp) * sin(hr)
             );
 } // end eulerToQuat
 
-qreal Math3D::quatToHeading(const QQuaternion& quat)
+QList<qreal> Math3D::quatToHPR(const QQuaternion& quat)
 {
-    qreal halfSinePitch = quat.x() * quat.y() + quat.z() * quat.scalar();
+	QList<qreal> hpr;
+	hpr << 0;
+	hpr << 0;
+	hpr << 0;
 
-    if(halfSinePitch == 0.5) // north pole
-    {
-        return 2 * atan2(quat.x(), quat.scalar());
-    }
-    else if(halfSinePitch == -0.5) // south pole
-    {
-        return -2 * atan2(quat.x(), quat.scalar());
-    }
-    else
-    {
-        return atan2(2 * quat.y() * quat.scalar() - 2 * quat.x() * quat.z(),
-                1 - 2 * pow(quat.y(), 2) - 2 * pow(quat.z(), 2));
-    } // end if
-} // end quatToHeading
+	quatToHPR(quat, &hpr[0], &hpr[1], &hpr[2]);
 
-qreal Math3D::quatToPitch(const QQuaternion& quat)
-{
-    return asin(2 * quat.x() * quat.y() + 2 * quat.z() * quat.scalar());
-} // end quatToPitch
-
-qreal Math3D::quatToRoll(const QQuaternion& quat)
-{
-    qreal halfSinePitch = quat.x() * quat.y() + quat.z() * quat.scalar();
-
-    if(halfSinePitch == 0.5) // north pole
-    {
-        return 0;
-    }
-    else if(halfSinePitch == -0.5) // south pole
-    {
-        return 0;
-    }
-    else
-    {
-        return atan2(2 * quat.x() * quat.scalar() - 2 * quat.y() * quat.z(),
-                1 - 2 * pow(quat.x(), 2) - 2 * pow(quat.z(), 2));
-    } // end if
-} // end quatToRoll
+	return hpr;
+} // end quatToHPR
 
 template<typename T> void Math3D::quatToHPR(const QQuaternion& quat, T* heading, T* pitch, T* roll)
 {
@@ -71,33 +43,54 @@ template<typename T> void Math3D::quatToHPR(const QQuaternion& quat, T* heading,
 	// If quaternion is normalised the unit is one, otherwise it is the correction factor
 	qreal unit = scalarSquared + xSquared + ySquared + zSquared;
 
-    qreal halfSinePitch = quat.x() * quat.z() - quat.y() * quat.scalar();
+    qreal halfSinePitch = quat.x() * quat.y() + quat.z() * quat.scalar();
 
 	*pitch = asin(2 * halfSinePitch);
 
     if(halfSinePitch + std::numeric_limits<double>::epsilon() >= 0.5 * unit) // north pole
     {
-        *heading = 2 * atan2(quat.y(), quat.scalar());
+        *heading = 2 * atan2(quat.x(), quat.scalar());
 
 		*roll = 0;
     }
     else if(halfSinePitch - std::numeric_limits<double>::epsilon() <= -0.5 * unit) // south pole
     {
-        *heading = -2 * atan2(quat.y(), quat.scalar());
+        *heading = -2 * atan2(quat.x(), quat.scalar());
 
 		*roll = 0;
     }
     else
     {
-		*heading = atan2(2 * (quat.x() * quat.scalar() + quat.y() * quat.z()), 1 - 2 * (zSquared + scalarSquared));
+		*heading = atan2(2 * (quat.scalar() * quat.y() - quat.x() * quat.z()), 1 - 2 * (ySquared + zSquared));
 
-		*roll = atan2(2 * (quat.x() * quat.y() + quat.z() * quat.scalar()), 1 - 2 * (ySquared + zSquared));
+		*roll = atan2(2 * (quat.scalar() * quat.x() - quat.y() * quat.z()), 1 - 2 * (xSquared + zSquared));
     } // end if
 } // end quatToHPR
 
 // Explicit template instantiations
 template void Math3D::quatToHPR<float>(const QQuaternion& quat, float* heading, float* pitch, float* roll);
 template void Math3D::quatToHPR<qreal>(const QQuaternion& quat, qreal* heading, qreal* pitch, qreal* roll);
+
+
+float Math3D::quatScalar(const QQuaternion& quat)
+{
+	return quat.scalar();
+} // end quatScalar
+
+float Math3D::quatX(const QQuaternion& quat)
+{
+	return quat.x();
+} // end quatX
+
+float Math3D::quatY(const QQuaternion& quat)
+{
+	return quat.y();
+} // end quatY
+
+float Math3D::quatZ(const QQuaternion& quat)
+{
+	return quat.z();
+} // end quatZ
 
 
 qreal Math3D::matrixToHeading(const QMatrix4x4& mat)
@@ -144,6 +137,22 @@ void Math3D::matrixToEuler(const QMatrix4x4& mat, qreal *heading, qreal *pitch, 
 
     *pitch = asin(mat(1, 2));
 } // end eulerToQuat
+
+
+QQuaternion Math3D::quatFromScalarAndVector(float scalar, const QVector3D& vector)
+{
+	return QQuaternion(scalar, vector);
+} // end quatFromScalarAndVector
+
+QVector3D Math3D::quatRotatedVector(const QQuaternion& quat, const QVector3D& vector)
+{
+	return quat.rotatedVector(vector);
+} // end quatRotatedVector
+
+QVector3D Math3D::quatVector(const QQuaternion& quat)
+{
+	return quat.vector();
+} // end quatVector
 
 
 QQuaternion Math3D::quatNormalized(const QQuaternion& quat)
@@ -199,3 +208,9 @@ QQuaternion Math3D::slerp(const QQuaternion& q1, const QQuaternion& q2, float t)
 {
 	return QQuaternion::slerp(q1, q2, t);
 } // end slerp
+
+
+float Math3D::vectorLength(const QVector3D& vec)
+{
+	return vec.length();
+} // end vectorLength
--- src/client/math3d.h
+++ src/client/math3d.h
@@ -12,15 +12,22 @@ class Math3D : public QObject
 
 public:
 	Q_INVOKABLE static QQuaternion eulerToQuat(qreal heading, qreal pitch, qreal roll);
-	Q_INVOKABLE static qreal quatToHeading(const QQuaternion& quat);
-	Q_INVOKABLE static qreal quatToPitch(const QQuaternion& quat);
-	Q_INVOKABLE static qreal quatToRoll(const QQuaternion& quat);
+	Q_INVOKABLE static QList<qreal> quatToHPR(const QQuaternion& quat);
 	template<typename T> static void quatToHPR(const QQuaternion& quat, T* heading, T* pitch, T* roll);
+
+	Q_INVOKABLE static float quatScalar(const QQuaternion& quat);
+	Q_INVOKABLE static float quatX(const QQuaternion& quat);
+	Q_INVOKABLE static float quatY(const QQuaternion& quat);
+	Q_INVOKABLE static float quatZ(const QQuaternion& quat);
 
 	Q_INVOKABLE static qreal matrixToHeading(const QMatrix4x4& mat);
 	Q_INVOKABLE static qreal matrixToPitch(const QMatrix4x4& mat);
 	Q_INVOKABLE static qreal matrixToRoll(const QMatrix4x4& mat);
 	static void matrixToEuler(const QMatrix4x4& mat, qreal *heading, qreal *pitch, qreal *roll);
+
+	Q_INVOKABLE static QQuaternion quatFromScalarAndVector(float scalar, const QVector3D& vector);
+	Q_INVOKABLE static QVector3D quatRotatedVector(const QQuaternion& quat, const QVector3D& vector);
+	Q_INVOKABLE static QVector3D quatVector(const QQuaternion& quat);
 
 	Q_INVOKABLE static QQuaternion quatNormalized(const QQuaternion& quat);
 	Q_INVOKABLE static QQuaternion quatConjugate(const QQuaternion& quat);
@@ -34,6 +41,8 @@ public:
 
 	Q_INVOKABLE static QQuaternion nlerp(const QQuaternion& q1, const QQuaternion& q2, float t);
 	Q_INVOKABLE static QQuaternion slerp(const QQuaternion& q1, const QQuaternion& q2, float t);
+
+	Q_INVOKABLE static float vectorLength(const QVector3D& vec);
 }; // end Math3D
 
 #endif // MATH3D_H
